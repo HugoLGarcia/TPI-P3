16243646c422c293a66a82a219ecd74bfe46ae3b
HugoLGarcia <hugoleonelg@gmail.com>
2026-05-08 18:14:00 -0300

    Cambios menores

--- src/app.js
+++ src/app.js
@@ -6,8 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/usuarios", usuariosRoutes);
-// rutas versionadas
-app.use("/api/v1/usuarios", usuariosRoutes);
+
 
 app.listen(3000, () => {
   console.log("Servidor corriendo en puerto 3000");
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,65 +1,18 @@
 import { Router } from "express";
-import { body, param } from "express-validator";
-import { validarCampos } from "../middlewares/validarCampos.js";
 import usuariosController from "../controllers/usuarios.controller.js";
+import { validarCorreo } from "../middlewares/validarCampos.js";
 
 const router = Router();
 
-// BREAD
-
-// Get All
+// Rutas de usuarios
 router.get("/", usuariosController.getAll);
-
-// GET BY ID con validación
-router.get(
-  "/:id",
-  [
-    param("id").isInt().withMessage("El ID debe ser numérico"),
-    validarCampos
-  ],
-  usuariosController.getById
-);
-
-// Create
-router.post(
-  "/",
-  [
-    body("documento").notEmpty().withMessage("Documento obligatorio"),
-    body("apellido").notEmpty().withMessage("Apellido obligatorio"),
-    body("nombres").notEmpty().withMessage("Nombres obligatorios"),
-    body("email").isEmail().withMessage("Email inválido"),
-    body("contrasenia")
-      .isLength({ min: 6 })
-      .withMessage("La contraseña debe tener al menos 6 caracteres"),
-    body("rol").notEmpty().withMessage("Rol obligatorio"),
-    validarCampos
-  ],
-  usuariosController.create
-);
-
-// Update
-router.put(
-  "/:id",
-  [
-    param("id").isInt().withMessage("El ID debe ser numérico"),
-    body("email").optional().isEmail().withMessage("Email inválido"),
-    body("contrasenia")
-      .optional()
-      .isLength({ min: 6 })
-      .withMessage("Mínimo 6 caracteres"),
-    validarCampos
-  ],
-  usuariosController.update
-);
-
-// Delete
-router.delete(
-  "/:id",
-  [
-    param("id").isInt().withMessage("El ID debe ser numérico"),
-    validarCampos
-  ],
-  usuariosController.remove
-);
+router.get("/buscar", usuariosController.search); //va antes de /:id, porque si no Express lo toma como un ID
+router.get("/:id", usuariosController.getById);
+router.post("/", usuariosController.create);
+router.put("/:id", usuariosController.update);
+router.delete("/:id", usuariosController.remove);
+router.delete("/estado/:id", usuariosController.changeStateId);
+router.patch("/correo/:id", validarCorreo, usuariosController.changeEmailId);
+router.post("/generico", usuariosController.registerGeneric);
 
 export default router;
