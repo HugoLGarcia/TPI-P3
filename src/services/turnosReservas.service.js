e5ebcfc73247b5ff03a6c83755232fa4236a92ba
juan puhl <juanpuhl7@gmail.com>
2026-06-10 21:35:36 -0300

    Validar turnos cada 15 minutos

--- src/services/turnosReservas.service.js
+++ src/services/turnosReservas.service.js
@@ -4,63 +4,54 @@ import pacientesService from "./pacientes.service.js";
 import obrasSocialesService from "./obrasSociales.service.js";
 
 const getAll = async (usuario) => {
-
   if (usuario.rol === 1) {
-    return await turnosReservasRepository.getTurnosByMedico(
-      usuario.id_usuario
-    );
+    return await turnosReservasRepository.getTurnosByMedico(usuario.id_usuario);
   }
 
-  return await turnosReservasRepository.getTurnosByPaciente(
-    usuario.id_usuario
-  );
+  return await turnosReservasRepository.getTurnosByPaciente(usuario.id_usuario);
 };
 
 const create = async (turnoReserva) => {
+  const fechaTurno = new Date(turnoReserva.fecha_hora);
+  const minutos = fechaTurno.getMinutes();
 
-  const medico = await medicosService.getById(
-    turnoReserva.id_medico
-  );
+  if (![0, 15, 30, 45].includes(minutos)) {
+    throw new Error("Los turnos solo pueden reservarse cada 15 minutos");
+  }
+
+  const medico = await medicosService.getById(turnoReserva.id_medico);
 
   if (!medico) {
     throw new Error("El médico no existe");
   }
 
-  const paciente = await pacientesService.getById(
-    turnoReserva.id_paciente
-  );
+  const paciente = await pacientesService.getById(turnoReserva.id_paciente);
 
   if (!paciente) {
     throw new Error("El paciente no existe");
   }
 
-  const turnoMedicoExiste =
-    await turnosReservasRepository.existeTurnoMedico(
-      turnoReserva.id_medico,
-      turnoReserva.fecha_hora
-    );
+  const turnoMedicoExiste = await turnosReservasRepository.existeTurnoMedico(
+    turnoReserva.id_medico,
+    turnoReserva.fecha_hora,
+  );
 
   if (turnoMedicoExiste) {
-    throw new Error(
-      "El médico ya tiene un turno asignado en ese horario"
-    );
+    throw new Error("El médico ya tiene un turno asignado en ese horario");
   }
 
   const turnoPacienteExiste =
     await turnosReservasRepository.existeTurnoPaciente(
       turnoReserva.id_paciente,
-      turnoReserva.fecha_hora
+      turnoReserva.fecha_hora,
     );
 
   if (turnoPacienteExiste) {
-    throw new Error(
-      "El paciente ya tiene un turno asignado en ese horario"
-    );
+    throw new Error("El paciente ya tiene un turno asignado en ese horario");
   }
 
-
   const obraSocial = await obrasSocialesService.getById(
-    paciente.id_obra_social
+    paciente.id_obra_social,
   );
 
   if (!obraSocial) {
@@ -70,8 +61,7 @@ const create = async (turnoReserva) => {
   let valor = Number(medico.valor_consulta);
 
   if (obraSocial.es_particular === 0) {
-    const descuento =
-      valor * (obraSocial.porcentaje_descuento / 100);
+    const descuento = valor * (obraSocial.porcentaje_descuento / 100);
 
     valor = valor - descuento;
   }
@@ -79,40 +69,25 @@ const create = async (turnoReserva) => {
   turnoReserva.valor_total = valor;
   turnoReserva.id_obra_social = paciente.id_obra_social;
 
-  return await turnosReservasRepository.create(
-    turnoReserva
-  );
+  return await turnosReservasRepository.create(turnoReserva);
 };
 
-const marcarAtendido = async (
-  id_turno,
-  id_usuario
-) => {
-
-  const turno =
-    await turnosReservasRepository.getTurnoById(
-      id_turno
-    );
+const marcarAtendido = async (id_turno, id_usuario) => {
+  const turno = await turnosReservasRepository.getTurnoById(id_turno);
 
   if (!turno) {
     throw new Error("Turno no encontrado");
   }
 
   if (turno.id_usuario !== id_usuario) {
-    throw new Error(
-      "No tiene permisos para modificar este turno"
-    );
+    throw new Error("No tiene permisos para modificar este turno");
   }
 
   if (turno.atentido === 1) {
-    throw new Error(
-      "El turno ya fue marcado como atendido"
-    );
+    throw new Error("El turno ya fue marcado como atendido");
   }
 
-  await turnosReservasRepository.marcarAtendido(
-    id_turno
-  );
+  await turnosReservasRepository.marcarAtendido(id_turno);
 
   return true;
 };
@@ -120,5 +95,5 @@ const marcarAtendido = async (
 export default {
   getAll,
   create,
-  marcarAtendido
+  marcarAtendido,
 };
